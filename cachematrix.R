ea4a6548fcb5e1b0bd9e0d7e6a7a6dc6d960adf7
Jason <strimp101@gmail.com>
2014-12-13 14:41:03 -0600

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,71 @@
-## Put comments here that give an overall description of what your
-## functions do
+## An n-by-n square matrix A is called invertible if there exists an n-by-n 
+## square matrix B such that \mathbf{AB} = \mathbf{BA} = \mathbf{I}_n \  
+## where In denotes the n-by-n identity matrix and the multiplication used 
+## is ordinary matrix multiplication 
+## (http://en.wikipedia.org/wiki/Invertible_matrix).
 
-## Write a short comment describing this function
+## Functions create a cacheable matrix object and computes the inverse while 
+## caching the inverse
+
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setinverse(m)
+    m
 }
+
+
+## Test whether a matrix A is invertible
+
+isMatrixInvertible <- function(A) {
+    rows <- nrow(A)
+    eye <- diag(rows)
+    B = solve(A)
+    all(A %*% B == eye)
+}
+
+## Example
+
+# create an invertible matrix
+A = rbind(c(1, -1/4), c(-1/4, 1))
+
+# simple test to confirm it's invertible 
+isMatrixInvertible(A)
+
+# get the special cacheable matrix object
+cachedMatrix <- makeCacheMatrix()
+
+# set the invertible matrix
+cachedMatrix$set(A)
+
+# since solved matrix is not cached, will not get the 'getting cached data'
+# message
+cacheSolve(cachedMatrix)
+
+# since solved matrix IS cached, will get the 'getting cached data' message
+cacheSolve(cachedMatrix)
